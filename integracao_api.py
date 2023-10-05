4b34695736d71f067f9c609f346f94fe07c965ff
Luiza Bryn <luizabryn00@gmail.com>
2023-10-05 15:50:02 -0300

    Update integracao_api.py
    Testando um problema mais simples antes de começar a pensar na assistente de empresa. Problema: categorizar animes pelo genero

--- integracao_api.py
+++ integracao_api.py
@@ -9,18 +9,41 @@ openai.api_base = "https://ciasc-openai.openai.azure.com/"
 openai.api_version = "2023-07-01-preview"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-resposta = openai.ChatCompletion.create(
-  engine="exercicio_azure_learn",
-  messages = [
-          {
-              "role":"system",
-              "content":"Você é uma assistente de uma empresa pública brasileira de tecnologia e automação de Santa Catarina chamada CIASC e seu nome é Ana, você gosta de fofoca e tem sotaque manézinho (original de Florianópolis)."
-          },
-          {
-              "role":"user",
-              "content":"Oi Ana, tudo bem? O que você anda ouvindo por aí?"
-          }
-  ]
-)
+def categorizaAnime(nome_do_anime, generos_validos):
+    prompt_sistema = f"""
+    Você é um categorizador de animes.
+    Você deve escolher um gênero da lista abaixo:
+    Se os gêneros informadas não forem gêneros válidos, responda com "Não posso ajudá-lo com isso"
+    ##### Lista de gêneros válidos
+    {generos_validos}
+    ##### Exemplo
+    One Piece
+    Shounen
+    """
+    resposta = openai.ChatCompletion.create(
+    engine="exercicio_azure_learn",
+    messages=[
+        {
+        "role": "system",
+        "content": prompt_sistema
+        },
+        {
+            "role": "user",
+            "content": nome_do_anime
+        }
+    ],
+    temperature=0.7,
+    max_tokens=800,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None,
+    )
+    print(resposta.choices[0].message.content)
 
-print(resposta)
+print("Digite as categorias validas:")
+generos_validos = input()
+while True:
+    print("Digite o nome do produto:")
+    nome_do_anime = input()
+    categorizaAnime(nome_do_anime, generos_validos)
